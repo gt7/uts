63c1330e99cd2a0e937b60651b3fdb7bb86c9fcc
mispan <>
2016-11-23 22:19:10 +0700

    12141417

---
+++ src/com/mispan/app12141417/App12141417.java
@@ -0,0 +1,11 @@
+
+package com.mispan.app12141417;
+import java.util.Random;
+import java.util.Scanner;
+
+public class App12141417 {
+    public static void main(String[] args) {
+        
+    }
+    
+}
