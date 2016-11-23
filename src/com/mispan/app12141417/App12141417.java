e90e63cd2a5b494d04cc700becbd563fbef0c3b4
mispan <root@DESKTOP-5GJ6NRA>
2016-11-23 22:23:45 +0700

    12141417 membuat variabel betul, skor, total dengan nilai 0 dan dengan tipe data integer

--- src/com/mispan/app12141417/App12141417.java
+++ src/com/mispan/app12141417/App12141417.java
@@ -5,7 +5,13 @@ import java.util.Scanner;
 
 public class App12141417 {
     public static void main(String[] args) {
+        int betul = 0;
+        int skor = 0;
+        int total = 0 ;
         
+        Scanner kbd = new Scanner(System.in);
+        System.out.print("Masukan nama Anda untuk memulai atau tekan y untuk keluar : ");
+        String nama = kbd.nextLine();
     }
     
 }
